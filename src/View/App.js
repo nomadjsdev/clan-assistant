8995ede5dcb921923407ede9cf01f4cb6caf6513
James <james+fork@stn.me.uk>
2019-12-21 22:29:21 +0700

    Add router & basic structure

--- src/View/App.js
+++ src/View/App.js
@@ -1,7 +1,26 @@
 import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+
+import Home from 'View/Home'
+import Register from 'View/Register'
+import Login from 'View/Login'
 
 const App = () => {
-  return <>App</>
+	return (
+		<Router>
+			<Switch>
+				<Route path="/register">
+					<Register />
+				</Route>
+				<Route path="/login">
+					<Login />
+				</Route>
+				<Route path="/">
+					<Home />
+				</Route>
+			</Switch>
+		</Router>
+	)
 }
 
 export default App
